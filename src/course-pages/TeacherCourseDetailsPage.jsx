5ad21ade048ea7e09402c36f2b154dda9ccc9292
gazizafor <gazi.zafor@therapservices.net>
2025-09-04 13:24:00 +0600

    Quiz question duplication bugfix

--- src/course-pages/TeacherCourseDetailsPage.jsx
+++ src/course-pages/TeacherCourseDetailsPage.jsx
@@ -305,7 +305,8 @@ const TeacherCourseDetailsPage = () => {
                 payload.resourceLink = formDataFromModal?.resourceLink;
 
             } else if (originalContentRelease?.type === 'QUIZ') {
-                payload.questions = [...(originalContentRelease?.questions || []), ...(formDataFromModal?.questions || [])];            }
+                payload.questions = [...(formDataFromModal?.questions || [])];
+            }
 
             // Capture the response from the publish API call
             const publishedContent = await publishContentRelease?.(payload?.id, payload);
